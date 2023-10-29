75da2599b54039a62e9b8da80a3b09f8077c1f08
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-10-28 21:23:39 -0400

    Modified product route to correct error

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   try {
     // Find a single product by its `id` and include associated Category and Tag data
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: "product_tags" }],
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id' });
